15762adf51b8df491b3969fd4c21bc58d5709ad0
rafat22 <rafat22@navgurukul.org>
2024-10-03 23:12:24 +0530

    responsive

--- app/components/Helppage.js
+++ app/components/Helppage.js
@@ -61,7 +61,7 @@ const Helppage = () => {
       <div className="flex max-w-6xl ml-[-491px] ">
         
         {/* Left Side - Text Section */}
-        <div className="flex flex-col w-1/2 p-8 mt-[35px] mr-[241px]">
+        <div className="flex flex-col w-1/2 p-8 mt-[35px] mr-[241px] xl:ml-[131px]">
           <h1 className="text-4xl font-bold mb-4 h-[30px] text-gray-500">You're Not Alone,</h1>
           <h1 className='text-[50px] text-[#1C8DCE] w-[509px] mt-[10px]'>We're Here To Help</h1>
           <p className="text-[20px] mb-4 text-black w-[700px] h-[48px] mt-[20px]">
@@ -100,7 +100,7 @@ const Helppage = () => {
           width={774}
           height={663}
           objectFit="cover"
-          className="mt-[53px] ml-[135px]" 
+          className="mt-[53px] ml-[135px] xl:ml-[-7px]" 
         />
       </div>
     </div>
